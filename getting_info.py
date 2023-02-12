961b2580b3f1e72e1a848f07c350ff8dc1584f49
Ahmed Almohamdy <115741333+ahmedalmohamdy@users.noreply.github.com>
2023-02-12 21:52:04 +0200

    implement examples function

--- getting_info.py
+++ getting_info.py
@@ -2,11 +2,11 @@ def examples():
     sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
     eq = int(input("Please enter the number of equation to be solved : "))
     file = open("user_manual.txt", 'w', encoding="utf-8")
-    file.write("This file is your catalogue to use this program.\n\n")
+    file.write("This file is your catalogue to use this program.\n\n\n")
     file.write(
         "First, Store your equations in a text file named 'equations.txt'.\n")
     file.write(
-        f"If you have {eq} equations, Your text file should be like this:\n")
+        f"\nIf you have {eq} equations, Your text file should be like this:\n\n")
     file.write(f"Number of equations: {eq}")
     for i in range(1, eq+1):
         file.write("\n")
@@ -21,7 +21,7 @@ def examples():
 
         file.write(f"a{i}0".translate(sub))
     file.write(
-        "\n\n###########################################################################################")
+        "\n\n\n##########################################################################################")
     file.close()
     print("The path of the file is : ",end=(" "))
     print(__file__.replace("getting_info.py", "user_manual.txt"))
