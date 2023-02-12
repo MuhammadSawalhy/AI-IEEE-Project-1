a9614926787e867c04b0dd0faaa739b752961a22
Ahmed Almohamdy <115741333+ahmedalmohamdy@users.noreply.github.com>
2023-02-12 20:45:15 +0200

    Update getting_info.py

--- getting_info.py
+++ getting_info.py
@@ -1,18 +1,18 @@
-from os import path
-
-DIRNAME = path.dirname(_file_)
-
 def examples():
     sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
     eq = int(input("Please enter the number of equation to be solved : "))
-    file = open("examples.txt", 'w')
+    file = open("user_manual.txt", 'w', encoding="utf-8")
     file.write("This file is your catalogue to use this program.\n\n")
-    file.write("First, Store your equations in a text file named 'equations.txt'.\n")
-    file.write(f"If you have {eq} equations, Your text file should be like this:\n")
+    file.write(
+        "First, Store your equations in a text file named 'equations.txt'.\n")
+    file.write(
+        f"If you have {eq} equations, Your text file should be like this:\n")
     file.write(f"Number of equations: {eq}")
     for i in range(1, eq+1):
         file.write("\n")
+        file.write(f"eq{i}: ")
         for j in range(1, eq+1):
+
             file.write(f"a{i}{j}x{j} ".translate(sub))
             if j < eq:
                 file.write("+ ")
@@ -20,8 +20,10 @@ def examples():
                 file.write("= ")
 
         file.write(f"a{i}0".translate(sub))
-    file.write("\n\n##########################################################################################")
+    file.write(
+        "\n\n##########################################################################################")
     file.close()
+    print(_file_.replace("getting_info.py", "examples.txt"))
 
 
 if _name_ == "_main_":
