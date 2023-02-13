dc9cb6a96fda841d46d1b390d3170474ca024fbf
alyalsayed <alyalsayed56@yahoo.com>
2023-02-13 22:20:07 +0200

    getting_equations done

--- getting_info.py
+++ getting_info.py
@@ -1,60 +1,97 @@
-# def examples():
-#     sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
-#     eq = int(input("Please enter the number of equation to be solved : "))
-#     file = open("user_manual.txt", 'w', encoding="utf-8")
-#     file.write("This file is your catalogue to use this program.\n\n")
-#     file.write(
-#         "First, Store your equations in a text file named 'equations.txt'.\n")
-#     file.write(
-#         f"If you have {eq} equations, Your text file should be like this:\n")
-#     file.write(f"Number of equations: {eq}")
-#     for i in range(1, eq+1):
-#         file.write("\n")
-#         file.write(f"eq{i}: ")
-#         for j in range(1, eq+1):
+def examples():
+    sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+    eq = int(input("Please enter the number of equation to be solved : "))
+    file = open("user_manual.txt", 'w', encoding="utf-8")
+    file.write("This file is your catalogue to use this program.\n\n")
+    file.write(
+        "First, Store your equations in a text file named 'equations.txt'.\n")
+    file.write(
+        f"If you have {eq} equations, Your text file should be like this:\n")
+    file.write(f"Number of equations: {eq}")
+    for i in range(1, eq+1):
+        file.write("\n")
+        file.write(f"eq{i}: ")
+        for j in range(1, eq+1):
 
-#             file.write(f"a{i}{j}x{j} ".translate(sub))
-#             if j < eq:
-#                 file.write("+ ")
-#             else:
-#                 file.write("= ")
+            file.write(f"a{i}{j}x{j} ".translate(sub))
+            if j < eq:
+                file.write("+ ")
+            else:
+                file.write("= ")
 
-#         file.write(f"a{i}0".translate(sub))
-#     file.write(
-#         "\n\n##########################################################################################")
-#     file.close()
-#     print(__file__.replace("getting_info.py", "user_manual.txt"))
-
-
-# def getting_equations():
-#     # Number of equations: 3
-#     # eq₁: 1x₁ + 3x₂ - 7x₃ = 0
-#     # eq₂: 8x₁ - 1x₂ + 3x₃ = -1
-#     # eq₃: -2x₁ + 10x₂ + 2x₃ = 2
-#     file = open("equations.txt", 'r', encoding="utf-8")
-#     lines = file.readlines()
-#     file.close()
-#     equations = []
-#     for line in lines:
-#         if line.startswith("eq"):
-#             data=line.split(":")[1].strip().split(" ")
-#             data = [i for i in data if i != "+" and i != "-" and i != "="]
-#             for list_ in data:
-#                 # [['-2', '₁₁'], ['10', '₂₁'], ['2', '₃'], ['2']]
-#                 if(len(list_)-1 == len(data)):
-#                     equations[u'{list_[1]}']=int(list_[0])
-#                 else:
-#                     for in range():
-#                         equations[u'{list_[1]}']=list_[0]
-#             #     for i in list_:
-#             #        #['1x₁', '+', '3x₂', '-', '7x₃', '=', '0']
-#             #        #  clean this to get {1:1,2:3,3:-7}
-#     return data
-#     #         equations.append(data)
-#     # return equations
+        file.write(f"a{i}0".translate(sub))
+    file.write(
+        "\n\n##########################################################################################")
+    file.close()
+    print(__file__.replace("getting_info.py", "user_manual.txt"))
 
 
-# if __name__ == "__main__":
-#     # examples()
-#     # print(getting_equations())
-#     print(getting_equations())
+def getting_equations():
+    file = open("equations.txt", 'r', encoding="utf-8")
+    line1 = file.readline()
+    no_eqs = int(line1.split(":")[1].strip())
+    # print(no_lines, type(no_lines))
+    lines = file.readlines()
+    file.close()
+    equations = []
+    SUB = str.maketrans("₀₁₂₃₄₅₆₇₈₉", "0123456789")
+    for line in lines:
+        equation = []
+        args = {}
+        if line.startswith("eq"):
+            data = line.split(":")[1].replace(" ", "")
+            for i in data:
+                if (i == '-'):
+                    data = data.replace(i, " -")
+                elif (i == '+' or i == '='):
+                    data = data.replace(i, " ")
+            data = data.split(" ")
+            # print(data)
+#               ['3x₂', '7x₃', '-1x₁', '0\n']
+#               ['3x₃', '8x₁', '', '-1x₂', '', '', '-1\n']
+#               ['', '-2x₁', '2x₃', '2']
+            d = int(data[-1])
+            factors = data[0:-1]
+            for var in (factors):
+                x = var.translate(SUB)
+                if (x != ''):
+                    equation.append(x)
+                # print(equation)
+#               ['3x2', '7x3', '-1x1']
+#               ['3x3', '8x1', '-1x2']
+#               ['-2x1', '2x3']
+            # if (len(equation) == no_eqs):
+            for value in equation:
+                args[int(value.split("x")[1])] = int(value.split("x")[0])
+
+            # solving problem of removed variables ex: 1x1 + 3x2 = 0
+            if (len(equation) < no_eqs):
+                for i in range(1, no_eqs+1):
+                    if i not in args:
+                        args[i] = 0
+            # print(args)
+    #        {2: 3, 3: 7, 1: -1}
+    #        {3: 3, 1: 8, 2: -1}
+    #        {1: -2, 3: 2, 2: 0}
+    #         # order keyes in dictionary
+    #         args = {k: args[k] for k in sorted(args)}
+    #         # print(args)
+    #     >> [[{1: -1, 2: 3, 3: 7}, 0], [{1: 8, 2: -1, 3: 3}, -1], [{1: -2, 2: 0, 3: 2}, 2]]
+            equations.append([args, d])
+            # print(equations)
+# [[{2: 3, 3: 7, 1: -1}, 0], [{3: 3, 1: 8, 2: -1}, -1], [{1: -2, 3: 2, 2: 0}, 2]]
+    # equations should be returned as 2D list
+            returned_equations = []
+            for list1 in equations:
+                eqs = []
+                for key, value in list1[0].items():
+                    eqs.append(value)
+                eqs.append(list1[1])
+                returned_equations.append(eqs)
+    # print(returned_equations)
+    # [[3, 7, -1, 0], [3, 8, -1, -1], [-2, 2, 0, 2]]
+    return returned_equations
+
+
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -27,65 +27,105 @@ def gauss_jordan_elimination(equations: list[list[int]]):
     non_zero = [-1] * n
     for i in range(n):
         eq = equations[i]
-        assert len(eq) == n + 1, "row length must equals no. of unkowns + 1"
-        for j in eq:
-            if j != 0:
+        if len(eq) != n + 1:
+            return {"type": soltype.none}
+        for j in range(len(eq) - 1):
+            if eq[j] != 0:
                 non_zero[j] = i
 
     for i in range(n):
         if equations[i][i] != 0:
             continue
         if non_zero[i] == -1:
-            # can't find a row with non-zero i-th element
-            return {"type": soltype.none}
+            # either no solutions or infinite number of them
+            continue
         add_rows(equations[i], equations[non_zero[i]], 1, 1)
 
     for i in range(n):
         for j in range(n):
-            if i == j:
+            if i == j or equations[i][i] == 0:
                 continue
-            add_rows(equations[j], equations[i], equations[i][i], -equations[j][i])
+            add_rows(equations[j], equations[i],
+                     equations[i][i], -equations[j][i])
 
     values = [0.0] * n
+
+    is_infinite = False
     for i in range(n):
+        if equations[i][i] == 0 and equations[i][n] == 0:
+            is_infinite = True
+            continue
         if equations[i][i] == 0:
-            return {"type": soltype.infinite}
+            return {"type": soltype.none}
         values[i] = equations[i][n] / equations[i][i]
+
+    if is_infinite:
+        return {"type": soltype.infinite}
 
     return {"type": soltype.only_one, "values": values}
 
 
+# if __name__ == "__main__":
+#     # # infinite number of solutions
+#     # eqs = [
+#     #     [1, 0, 0],
+#     #     [2, 0, 0],
+#     # ]
+#     # print(gauss_jordan_elimination(eqs))
+#     # # no solution exists
+#     # eqs = [
+#     #     [1, 0, 1],
+#     #     [2, 0, 1],
+#     # ]
+#     # print(gauss_jordan_elimination(eqs))
+#     # # no solution
+#     # eqs = [
+#     #     [1, 1, 1, 1],
+#     #     [1, 1, 1, 2],
+#     #     [0, 0, 0, 0],
+#     # ]
+#     # print(gauss_jordan_elimination(eqs))
+#     # # no solution
+#     # eqs = [
+#     #     [1, 0, 0, 1],
+#     #     [1, 0, 0, 2],
+#     #     [0, 0, 0, 0],
+#     # ]
+#     # print(gauss_jordan_elimination(eqs))
+
+
+def main():
+    # =======================================================================
+    # Writing subscripts into files
+    # option 1:
+    # \u0x208N for numbers, +, -, =, (, ) (N goes from 0 to F)
+    # \u0x209N for letters
+    # ex: print(u'H\u2082O\u2082')    >> H₂O₂
+    # option 2:
+    # using str.maketrans(), translate() methods
+    # =======================================================================
+    solution = gauss_jordan_elimination()
+    file_solution = open("solution.txt", "w", encoding="utf-8")
+    if solution["type"] == 1:  # one solution
+        file_solution.write("There is one solution\n")
+        for i, ans in enumerate(solution["values"]):
+            string = 'X'+f"{i}"
+            SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+            file_solution.write(
+                f"{string.translate(SUB)}" + " = " + str(ans)+"\n")
+    elif solution["type"] == 2:  # infinite solution
+        file_solution.write("There is infinite solution\n")
+    elif solution["type"] == 0:  # no solution
+        file_solution.write("There is no solution\n")
+    file_solution.close()
+    # __file__ print path of of main.py which is the same as solution.txt
+    print(__file__.replace("main.py", "solution.txt"))
+
+
 if __name__ == "__main__":
     eqs = [
-        [1, 4, 12],
+        [3, 4, 12],
         [6, 8, 24],
     ]
     ans = gauss_jordan_elimination(eqs)
     print(ans)
-
-# def main():
-#     # =======================================================================
-#     # Writing subscripts into files
-#     # option 1:
-#     # \u0x208N for numbers, +, -, =, (, ) (N goes from 0 to F)
-#     # \u0x209N for letters
-#     # ex: print(u'H\u2082O\u2082')    >> H₂O₂
-#     # option 2:
-#     # using str.maketrans(), translate() methods
-#     # =======================================================================
-#     solution = gauss_jordan_elimination()
-#     file_solution = open("solution.txt", "w", encoding="utf-8")
-#     if solution["type"]== 1:  # one solution
-#         file_solution.write("There is one solution:\n")
-#         for i, ans in enumerate(solution["values"]):
-#             string = 'X'+f"{i}"
-#             SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
-#             file_solution.write(
-#                 f"{string.translate(SUB)}" + " = " + str(ans)+"\n")
-#     elif solution["type"]== 2:  # infinite solution
-#         file_solution.write(solution["infinite"])
-#     elif solution["type"]== 0:  # no solution
-#         file_solution.write(solution["none"])
-#     file_solution.close()
-#     # __file__ print path of of main.py which is the same as solution.txt
-#     print(__file__.replace("main.py", "solution.txt"))
